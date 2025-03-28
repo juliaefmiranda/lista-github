dd6a81d36835138848e5d3a0851e89ca462b3ff7
Julia <jerlo180908@gmail.com>
2025-03-28 11:44:55 -0300

    feat(operadores-aritmeticos): adiciona divisão de valor entre 4 pessoas

--- divisãodegrana.js
+++ divisãodegrana.js
@@ -2,4 +2,4 @@ let dinheiro = 80;
 let pessoas = 4;
 let valorDividido = dinheiro / pessoas;
 
-console.log(`Cada pessoa leva ${valorDividido} reais.`)
+console.log('Cada pessoa leva', valorDividido, 'reais.')
--- quadradoecubo.js
+++ quadradoecubo.js
@@ -3,4 +3,4 @@ let num = 4;
 let numAoCubo = num * num * num;
 let numAoQuadrado = num * num;
 
-console.log(num,  'ao quadrado é igual a', numAoQuadrado, 'e' ,num ,'ao cubo é igual a', numAoCubo)
+console.log(num, 'ao quadrado é igual a', numAoQuadrado, 'e' ,num ,'ao cubo é igual a', numAoCubo)
